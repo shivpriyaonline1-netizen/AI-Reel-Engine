69c00f5e65a146b704a9c9c6d1df2d6001f0030f
SHIVPRIYA ONLINE <shivpriyaonline1@gmail.com>
2026-07-02 01:17:34 +0530

    Added Content API Debug

--- src/routes/contentRoutes.js
+++ src/routes/contentRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const contentController = require("../controllers/contentController");
 
+console.log("CONTENT ROUTES LOADED");
+
 router.get("/:id", contentController.get);
 
 module.exports = router;
