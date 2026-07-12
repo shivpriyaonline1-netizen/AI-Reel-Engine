bb4cb6221ab8bbf4dd0890dd0c376ba6ba741b88
SHIVPRIYA ONLINE <shivpriyaonline1@gmail.com>
2026-07-12 17:24:29 +0530

    Fix CORS for WordPress dashboard

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const path = require("path");
 const uploadRoute = require("./src/routes/upload");
 
 const express = require("express");
+const cors = require("cors");
 const routes = require("./src/routes");
 
 const app = express();
+
+app.use(cors({
+    origin: "https://shivpriyaonline.com"
+}));
 
 app.use(express.json());
 app.use(
